dd1a495bb66a1a232ef8e85806f1359ea820c5d3
Thang Tong <tongthang0104@gmail.com>
2015-10-28 23:34:04 -0600

    fix some weather.swift properties

--- Weather/Weather.swift
+++ Weather/Weather.swift
@@ -10,6 +10,8 @@ import Foundation
 import UIKit
 
 class Weather {
+    
+    static let WeatherKey = "weather"
     static let cityNameKey = "name"
     static let tempCKey = "temp"
     static let mainKey = "main"
@@ -17,32 +19,44 @@ class Weather {
     static let iconImageStringKey = "icon"
     
     var cityName: String = ""
-    var tempC: Float? = 0.0
     var main: String = ""
     var description: String = ""
     var iconImageString: String? = ""
     var iconImage: UIImage?
-    
-    init(jsonDictionary: [String: AnyObject]) {
-        if let name = jsonDictionary[Weather.cityNameKey] as? String {
-            self.cityName = name
-        }
-        if let tempC = jsonDictionary[Weather.tempCKey] as? Float {
-            self.tempC = tempC
-        }
-        if let main = jsonDictionary[Weather.mainKey] as? String {
-            self.main = main
-        }
-        if let description = jsonDictionary[Weather.descriptionKey] as? String {
-            self.description = description
-        }
-        if let iconImageString = jsonDictionary[Weather.iconImageStringKey] as? String {
-            self.iconImageString = iconImageString
+    var tempK: Float?
+    var tempC: Float? {
+        get {
+            if let tempK = tempK {
+                return tempK - 273.15
+            }else {
+                return nil
+            }
         }
     }
     
-    
-    
+    init(jsonDictionary: [String: AnyObject]) {
+        if let cityName = jsonDictionary[Weather.cityNameKey] as? String {
+            self.cityName = cityName
+        }
+        if let main = jsonDictionary[Weather.mainKey] as? [String: AnyObject] {
+            if let temperature = main[Weather.tempCKey] as? NSNumber {
+                self.tempK = Float(temperature)
+            }
+            
+            if let arrayUsingWeatherKey = jsonDictionary[Weather.WeatherKey] as? [[String : AnyObject]] {
+                if let description = arrayUsingWeatherKey[0] [Weather.descriptionKey] as? String {
+                    self.description = description
+                }
+                
+                if let iconImageString = arrayUsingWeatherKey[0] [Weather.iconImageStringKey] as? String {
+                    self.iconImageString = iconImageString
+                }
+                if let main = arrayUsingWeatherKey[0][Weather.WeatherKey] as? String {
+                    self.main = main
+                }
+            }
+        }
+    }
 }
 
 
